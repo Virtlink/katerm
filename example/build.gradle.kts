ecb79c27aecddd89846a37bc93544298aa8371cd
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-06-03 18:16:01 +0200

    Add example project for KSP

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,20 +1,16 @@
 plugins {
     `java-library`
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 dependencies {
-    // KSP
-    implementation      (libs.ksp.api)
+    implementation      (project(":katerm"))
+    implementation      (project(":katerm-ksp"))
+    ksp                 (project(":katerm-ksp"))
 
     // Testing
     testImplementation  (libs.kotest)
     testImplementation  (libs.kotest.datatest)
     testImplementation  (libs.kotest.property)
-}
-
-buildscript {
-    dependencies {
-        classpath(kotlin("gradle-plugin", version = "2.1.21"))
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@ rootProject.name = "katerm-project"
 
 include(":katerm")
 include(":katerm-ksp")
+include(":example")
 
