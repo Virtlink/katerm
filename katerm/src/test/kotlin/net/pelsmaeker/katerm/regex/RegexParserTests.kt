a873072b92c8e8e1ca2aedbf397475aea84cd9e6
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-09-29 18:32:46 +0200

    Add property-based test

--- katerm/src/test/kotlin/net/pelsmaeker/katerm/regex/RegexParserTests.kt
+++ katerm/src/test/kotlin/net/pelsmaeker/katerm/regex/RegexParserTests.kt
@@ -6,8 +6,36 @@ import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.RandomSource
 import io.kotest.property.arbitrary.ArbitraryBuilderContext
+import io.kotest.property.arbitrary.Codepoint
+import io.kotest.property.arbitrary.alphanumeric
 import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.codepoints
 import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
+
+
+val regexArb: Arb<RegexAst> = arbitrary { rs: RandomSource ->
+    suspend fun ArbitraryBuilderContext.simpleTerm(): RegexAst {
+        val str = Arb.string(minSize = 1, maxSize = 6, codepoints = Codepoint.alphanumeric()).bind()
+        val matcher = EqualityMatcher<String, Unit>(str)
+        return RegexAst.Builder.atom(matcher)
+    }
+    suspend fun ArbitraryBuilderContext.recursive(depth: Int): RegexAst {
+        if (depth <= 0) return simpleTerm()
+        return when (rs.random.nextInt(0, 7)) {
+            0 -> RegexAst.Builder.concat(recursive(depth - 1), recursive(depth - 1))
+            1 -> RegexAst.Builder.union(recursive(depth - 1), recursive(depth - 1))
+            2 -> RegexAst.Builder.star(recursive(depth - 1))
+            3 -> RegexAst.Builder.plus(recursive(depth - 1))
+            4 -> RegexAst.Builder.question(recursive(depth - 1))
+            5 -> RegexAst.Builder.starLazy(recursive(depth - 1))
+            6 -> RegexAst.Builder.plusLazy(recursive(depth - 1))
+            7 -> RegexAst.Builder.questionLazy(recursive(depth - 1))
+            else -> simpleTerm()
+        }
+    }
+    recursive(rs.random.nextInt(1, 8))
+}
 
 class RegexParserTests: FunSpec({
 
@@ -31,28 +59,24 @@ class RegexParserTests: FunSpec({
         }
     }
 
-    val regexArb: Arb<RegexAst> = arbitrary { rs: RandomSource ->
-        suspend fun ArbitraryBuilderContext.simpleTerm(): RegexAst {
-            val str = Arb.string(minSize = 1, codepoints = ).bind() // TODO: Remove space, slash, and other codepoints
-//            val str = with(ctx) { Arb.string(minSize = 1).bind() }
-            val matcher = EqualityMatcher<String, Unit>(str)
-            return RegexAst.Builder.atom(matcher)
-        }
-        suspend fun ArbitraryBuilderContext.recursive(depth: Int): RegexAst {
-            if (depth <= 0) return simpleTerm()
-            return when (rs.random.nextInt(0, 7)) {
-                0 -> RegexAst.Builder.concat(recursive(depth - 1), recursive(depth - 1))
-                1 -> RegexAst.Builder.union(recursive(depth - 1), recursive(depth - 1))
-                2 -> RegexAst.Builder.star(recursive(depth - 1))
-                3 -> RegexAst.Builder.plus(recursive(depth - 1))
-                4 -> RegexAst.Builder.question(recursive(depth - 1))
-                5 -> RegexAst.Builder.starLazy(recursive(depth - 1))
-                6 -> RegexAst.Builder.plusLazy(recursive(depth - 1))
-                else -> RegexAst.Builder.questionLazy(recursive(depth - 1))
+
+    test("property should succeed") {
+        checkAll(regexArb) { regex ->
+            // Arrange
+            val regexStr = regex.toString()
+            val parser = RegexParser<RegexAst, String, Unit>(
+                builder = RegexAst.Builder,
+                tokenMatcherBuilder = { token, _ -> EqualityMatcher(token) },
+            )
+
+            // Act
+            val parsedRegex = parser.parse(regexStr)
+
+            // Assert
+            withClue("Parsed regex: $regexStr") {
+                parsedRegex shouldBe regex
             }
         }
-        recursive(rs.random.nextInt(1, 4))
-
     }
 
 })
