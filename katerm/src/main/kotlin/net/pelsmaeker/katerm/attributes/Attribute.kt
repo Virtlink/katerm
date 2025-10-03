d9739b9db5df1b8c89da21d085f5252b222d3376
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2025-10-03 13:29:10 +0200

    Move Attributes to lsp4k

--- katerm/src/main/kotlin/net/pelsmaeker/katerm/attributes/Attribute.kt
+++
@@ -1,8 +0,0 @@
-package net.pelsmaeker.katerm.attributes
-
-/**
- * An attribute that can be associated with an object.
- *
- * @param T The type of value of the attribute.
- */
-abstract class Attribute<out T>
